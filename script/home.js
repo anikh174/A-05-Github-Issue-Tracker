eb3d8ea7be70f8de0870c030411a6e850d5dcc76
anikh174 <hossainanik174@gmail.com>
2026-03-10 10:12:22 +0600

    button functionality added

--- script/home.js
+++ script/home.js
@@ -20,23 +20,23 @@ const displayCards = cards => {
      <div class="bg-white shadow-sm items-center space-y-5 p-4 cursor-pointer ${(card.status)}"">
           <div class="flex justify-between items-center">
             <img class="w-6 h-6" src="./assets/Open-Status.png" alt="">
-            <div class="${(card.status)} p-2 rounded-2xl">
+            <div class="${(card.status)} p-2">
               <h2 class="font-bold">${card.status}</h2>
             </div>
           </div>
 
           <div class="space-y-4">
-            <h2 class="line-clamp-1 font-bold text-2xl">${card.title}</h2>
-            <p class="line-clamp-2 text-gray-400">${card.description}</p>
-            <div class="flex gap-3">
+            <h2 class="font-semibold text-[#1F2937] line-clamp-1  text-sm">${card.title}</h2>
+            <p class="line-clamp-2 text-[#64748B] text-[12px]">${card.description}</p>
+            <div class="flex justify-between">
               ${(card.labels)}
             </div>
           </div>
         </div>
 
         <div class="shadow-md p-4">
-          <p class="text-gray-400">${card.author? card.author: "not found"}</p>
-          <p class="text-gray-400">${card.createdAt}</p>
+          <p class="text-[#64748B]">${card.author? card.author: "not found"}</p>
+          <p class="text-[#64748B]">${card.createdAt}</p>
         </div>
     `;
     allCards.appendChild(Div);
@@ -44,4 +44,16 @@ const displayCards = cards => {
 };
 
 loadedData();
+const allBtn = () => {
+  displayCards(allIssue);
+};
+const openBtn = () => {
+  const openCards = allIssue.filter(card => card.status === 'open');
+  displayCards(openCards);
+};
+
+const closedBtn = () => {
+  const closedCards = allIssue.filter(card => card.status === 'closed');
+  displayCards(closedCards);
+};
 
